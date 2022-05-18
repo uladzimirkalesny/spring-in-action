fdbb014031119fe1c54db195fb1c5d2e8d8fd205
Uladzimir Kalesny <uladzimirkalesny@gmail.com>
2022-05-18 23:45:56 +0400

    [request-handling] added HomeController, HomeControllerTest, home.html page

---
+++ src/test/java/com/github/uladzmirkalesny/springinaction/shawermapatrol/HomeControllerTest.java
@@ -0,0 +1,28 @@
+package com.github.uladzmirkalesny.springinaction.shawermapatrol;
+
+import lombok.SneakyThrows;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(controllers = {HomeController.class})
+class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void testHomePage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(containsString("Welcome to Shawerma Patrol")));
+    }
+}
